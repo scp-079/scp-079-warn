921c3acac8b4a944c320c6dfbb5bded203aff8fc
Xiao <xiao@scp-079.org>
2019-04-27 05:42:47 +0000

    0.0.7

--- main.py
+++ main.py
@@ -46,8 +46,8 @@ app.start()
 
 # Timer
 scheduler = BackgroundScheduler()
-scheduler.add_job(backup_files, "cron", [app], hour=18)
-scheduler.add_job(update_status, "cron", [app], hour=18, minute=30)
+scheduler.add_job(backup_files, "cron", [app], hour=20)
+scheduler.add_job(update_status, "cron", [app], hour=20, minute=30)
 scheduler.add_job(reset_data, "cron", day=glovar.reset_day, hour=22)
 scheduler.add_job(update_admins, "cron", [app], hour=22, minute=30)
 scheduler.start()
