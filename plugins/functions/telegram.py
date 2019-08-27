3ceb4a0d5aeb4dd4834c22e6984036634c71b5d5
Xiao <xiao@scp-079.org>
2019-08-27 09:01:59 +0000

    - 稍加修改

--- plugins/functions/telegram.py
+++ plugins/functions/telegram.py
@@ -147,7 +147,7 @@ def get_group_info(client: Client, chat: Union[int, Chat]) -> (str, str):
         if chat.username:
             group_link = "https://t.me/" + chat.username
     except Exception as e:
-        logger.warning(f"Get group title error: {e}", exc_info=True)
+        logger.info(f"Get group info error: {e}", exc_info=True)
 
     return group_name, group_link
 
