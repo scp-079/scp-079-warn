9752552bbed5c51dee6f11db35efe6596de073b6
Xiao <xiao@scp-079.org>
2019-08-28 08:00:01 +0000

    - 稍加修改

--- plugins/functions/receive.py
+++ plugins/functions/receive.py
@@ -63,7 +63,7 @@ def receive_config_reply(client: Client, data: dict) -> bool:
             [
                 [
                     InlineKeyboardButton(
-                        "前往设置",
+                        text="前往设置",
                         url=link
                     )
                 ]
