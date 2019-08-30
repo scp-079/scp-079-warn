4d4800291abcdd44bd9c8c2419636cbbbec0a996
Xiao <xiao@scp-079.org>
2019-08-30 10:12:43 +0000

    - 修复 message_ids 没有长效化的 bug

--- plugins/functions/group.py
+++ plugins/functions/group.py
@@ -68,6 +68,9 @@ def leave_group(client: Client, gid: int) -> bool:
         glovar.configs.pop(gid, None)
         save("configs")
 
+        glovar.message_ids.pop(gid, 0)
+        save("message_ids")
+
         return True
     except Exception as e:
         logger.warning(f"Leave group error: {e}", exc_info=True)
