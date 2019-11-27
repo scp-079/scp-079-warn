c0e9d3594c7e8bd0106e7808870541f00089f008
Xiao <xiao@scp-079.org>
2019-11-27 13:17:44 +0000

    - 0.3.3

--- main.py
+++ main.py
@@ -36,16 +36,23 @@ app = Client(
     session_name="bot",
     bot_token=glovar.bot_token
 )
+app.start()
+
+# Send online status
+update_status(app, "online")
 
 # Timer
-scheduler = BackgroundScheduler()
+scheduler = BackgroundScheduler(job_defaults={"misfire_grace_time": 60})
 scheduler.add_job(interval_hour_01, "interval", [app], hours=1)
-scheduler.add_job(update_status, "cron", [app], minute=30)
+scheduler.add_job(update_status, "cron", [app, "awake"], minute=30)
 scheduler.add_job(backup_files, "cron", [app], hour=20)
 scheduler.add_job(update_report_ids, "cron", [app], hour=21, minute=30)
 scheduler.add_job(reset_data, "cron", day=glovar.reset_day, hour=22)
 scheduler.add_job(update_admins, "cron", [app], hour=22, minute=30)
 scheduler.start()
 
-# Run
-app.run()
+# Hold
+app.idle()
+
+# Stop
+app.stop()
