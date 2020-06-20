d189a48549d03b66ed3c05f45716006033652e9b
Xiao <xiao@scp-079.org>
2020-06-20 04:49:42 +0000

    增加 version 信息

--- plugins/functions/etc.py
+++ plugins/functions/etc.py
@@ -17,12 +17,13 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+from datetime import datetime
 from html import escape
 from json import dumps, loads
 from random import choice, uniform
 from string import ascii_letters, digits
 from threading import Thread, Timer
-from time import sleep, time
+from time import localtime, sleep, strftime, time
 from typing import Any, Callable, List, Optional, Union
 
 from cryptography.fernet import Fernet
@@ -87,9 +88,10 @@ def code_block(text: Any) -> str:
     return ""
 
 
-def crypt_str(operation: str, text: str, key: str) -> str:
+def crypt_str(operation: str, text: str, key: bytes) -> str:
     # Encrypt or decrypt a string
     result = ""
+
     try:
         f = Fernet(key)
         text = text.encode("utf-8")
@@ -271,6 +273,21 @@ def get_now() -> int:
         result = int(time())
     except Exception as e:
         logger.warning(f"Get now error: {e}", exc_info=True)
+
+    return result
+
+
+def get_readable_time(secs: int = 0, the_format: str = "%Y%m%d%H%M%S") -> str:
+    # Get a readable time string
+    result = ""
+
+    try:
+        if secs:
+            result = datetime.utcfromtimestamp(secs).strftime(the_format)
+        else:
+            result = strftime(the_format, localtime())
+    except Exception as e:
+        logger.warning(f"Get readable time error: {e}", exc_info=True)
 
     return result
 
--- plugins/handlers/command.py
+++ plugins/handlers/command.py
@@ -19,13 +19,15 @@
 import logging
 import re
 from copy import deepcopy
+from subprocess import run, PIPE
 
 from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup, Message
 
 from .. import glovar
 from ..functions.channel import get_debug_text, share_data
-from ..functions.etc import bold, button_data, code, delay, get_callback_data, get_command_context, get_command_type
-from ..functions.etc import get_full_name, get_int, get_now, lang, mention_id, thread
+from ..functions.etc import button_data, code, delay, general_link, get_callback_data, get_command_context
+from ..functions.etc import get_command_type, get_full_name, get_int, get_now, get_readable_time, lang, mention_id
+from ..functions.etc import thread
 from ..functions.file import save
 from ..functions.filters import authorized_group, class_d, from_user, is_class_c, is_watch_user, is_high_score_user
 from ..functions.filters import is_class_e_user, test_group
@@ -704,24 +706,44 @@ def undo(client: Client, message: Message) -> bool:
                    & from_user)
 def version(client: Client, message: Message) -> bool:
     # Check the program's version
+    result = False
+
     try:
         # Basic data
         cid = message.chat.id
         aid = message.from_user.id
         mid = message.message_id
 
+        # Get command type
+        command_type = get_command_type(message)
+
+        # Check the command type
+        if command_type and command_type.upper() != glovar.sender:
+            return False
+
+        # Version info
+        git_change = bool(run("git diff-index HEAD --", stdout=PIPE, shell=True).stdout.decode().strip())
+        git_date = run("git log -1 --format='%at'", stdout=PIPE, shell=True).stdout.decode()
+        git_date = get_readable_time(get_int(git_date), "%Y/%m/%d %H:%M:%S")
+        git_hash = run("git rev-parse --short HEAD", stdout=PIPE, shell=True).stdout.decode()
+        get_hash_link = f"https://github.com/scp-079/scp-079-{glovar.sender.lower()}/commit/{git_hash}"
+        command_date = get_readable_time(message.date, "%Y/%m/%d %H:%M:%S")
+
         # Generate the text
         text = (f"{lang('admin')}{lang('colon')}{mention_id(aid)}\n\n"
-                f"{lang('version')}{lang('colon')}{bold(glovar.version)}\n")
+                f"{lang('project')}{lang('colon')}{code(glovar.sender)}\n"
+                f"{lang('version')}{lang('colon')}{code(glovar.version)}\n"
+                f"{lang('本地修改')}{lang('colon')}{code(git_change)}\n"
+                f"{lang('哈希值')}{lang('colon')}{general_link(git_hash, get_hash_link)}\n"
+                f"{lang('提交时间')}{lang('colon')}{code(git_date)}\n"
+                f"{lang('命令发送时间')}{lang('colon')}{code(command_date)}\n")
 
         # Send the report message
-        thread(send_message, (client, cid, text, mid))
-
-        return True
+        result = send_message(client, cid, text, mid)
     except Exception as e:
         logger.warning(f"Version error: {e}", exc_info=True)
 
-    return False
+    return result
 
 
 @Client.on_message(Filters.incoming & Filters.group & Filters.command(["warn"], glovar.prefix)
