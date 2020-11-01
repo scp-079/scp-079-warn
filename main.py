342be45af97c1ee1c6cff306794ed79b61639fd3
Xiao <xiao@scp-079.org>
2020-11-01 06:31:58 +0000

    update

--- main.py
+++ main.py
@@ -20,6 +20,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
+from random import randint
 
 from apscheduler.schedulers.background import BackgroundScheduler
 from pyrogram import Client
@@ -44,7 +45,7 @@ update_status(app, "online")
 # Timer
 scheduler = BackgroundScheduler(job_defaults={"misfire_grace_time": 60})
 scheduler.add_job(interval_hour_01, "interval", [app], hours=1)
-scheduler.add_job(update_status, "cron", [app, "awake"], minute=30)
+scheduler.add_job(update_status, "cron", [app, "awake"], minute=randint(30, 34), second=randint(0, 59))
 scheduler.add_job(backup_files, "cron", [app], hour=20)
 scheduler.add_job(update_report_ids, "cron", [app], hour=21, minute=30)
 scheduler.add_job(reset_data, "cron", [app], day=glovar.date_reset, hour=22)
